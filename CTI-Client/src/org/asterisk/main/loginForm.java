52d0b70c7c83020f32d298240f9763fa80c084c5
haithanh.uit <haithanh.uit@gmail.com>
2012-09-27 05:12:33 +0000

    cti client form

---
+++ CTI-Client/src/org/asterisk/main/loginForm.java
@@ -0,0 +1,221 @@
+package org.asterisk.main;
+
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import org.asterisk.utility.*;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPasswordField;
+import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class loginForm {
+
+	private JFrame frmLoginFormAgent;
+	private JTextField textAgent;
+	private JTextField textExtension;
+	private JPasswordField passwordField;
+	private JTextField textQueue;
+	private JLabel lblAgentUser;
+	private JLabel lblPassword;
+	private JLabel lblExtension;
+	private JLabel lblQueue;
+	private JLabel lblStatus;
+	private JButton btnClear;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					loginForm window = new loginForm();
+					window.frmLoginFormAgent.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public loginForm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmLoginFormAgent = new JFrame();
+		frmLoginFormAgent.setResizable(false);
+		frmLoginFormAgent.setTitle("Login Form Agent");
+		frmLoginFormAgent.setBounds(100, 100, 589, 344);
+		frmLoginFormAgent.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String agent = textAgent.getText();
+				String pass = passwordField.getText();
+				String exten = textExtension.getText();
+				String queue = textQueue.getText();
+				String command = agent+"@"+pass+"@"+exten+"@"+queue;
+				new Agent("localhost", 22222);
+				lblStatus.setText(command);
+			}
+		});
+		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		textAgent = new JTextField();
+		textAgent.setColumns(10);
+		
+		textExtension = new JTextField();
+		textExtension.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		
+		textQueue = new JTextField();
+		textQueue.setColumns(10);
+		
+		lblAgentUser = new JLabel("Agent User");
+		
+		lblPassword = new JLabel("Password\r\n");
+		
+		lblExtension = new JLabel("Extension");
+		
+		lblQueue = new JLabel("Queue");
+		
+		lblStatus = new JLabel("status");
+		
+		btnClear = new JButton("Clear");
+		btnClear.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				textAgent.setText("");
+				textExtension.setText("");
+				textQueue.setText("");
+				passwordField.setText("");
+			}
+		});
+		btnClear.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JList list = new JList();
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"sip1", "sip2", "sip3"}));
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				
+			}
+		});
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"queue1", "queue2", "queue3", "queue4", "queue5", "queue6"}));
+		GroupLayout groupLayout = new GroupLayout(frmLoginFormAgent.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(55)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblStatus, GroupLayout.PREFERRED_SIZE, 414, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(lblQueue, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblPassword, GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE)
+								.addComponent(lblAgentUser, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(lblExtension, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+										.addComponent(passwordField, Alignment.LEADING, 133, 133, 133)
+										.addComponent(textAgent, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
+										.addComponent(textExtension, GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE))
+									.addGap(70)
+									.addComponent(list, GroupLayout.PREFERRED_SIZE, 1, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(textQueue, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
+									.addGap(79)))
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
+								.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE))
+							.addGap(68))))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(129)
+					.addComponent(btnLogin, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)
+					.addGap(31)
+					.addComponent(btnClear, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(231, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblStatus)
+					.addGap(27)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textAgent, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblAgentUser))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblPassword))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textExtension, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblExtension)
+						.addComponent(list, GroupLayout.PREFERRED_SIZE, 1, GroupLayout.PREFERRED_SIZE)
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textQueue, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblQueue)
+						.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(31)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnLogin, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnClear, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE))
+					.addGap(26))
+		);
+		frmLoginFormAgent.getContentPane().setLayout(groupLayout);
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+}
