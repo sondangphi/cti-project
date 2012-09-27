790bb1e44bcfc77c5c3a49ff5c01273331a2a563
haithanh.uit@gmail.com <haithanh.uit@gmail.com@de237bcd-42ee-5135-ae50-1b13d09ddbe3>
2012-09-27 05:12:33 +0000

    cti client form

---
+++ CTI-Client/src/org/asterisk/utility/Agent.java
@@ -0,0 +1,144 @@
+package org.asterisk.utility;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+public class Agent implements Runnable{
+
+	/**
+	 * @param args
+	 */
+	static boolean running = true;
+	static boolean connected = true;
+	static Thread mainThread;
+	static Socket clientSocket;
+	static BufferedReader infromServer;
+	static PrintWriter outtoServer;
+	static BufferedReader inputLine ;
+	int data;
+	
+	public Agent(){
+		
+	}
+
+	public Agent(String address, int port){
+		try{
+			clientSocket = new Socket(address, port);	
+			mainThread = new Thread(this);
+			mainThread.start();
+		}catch(Exception ex){
+			
+		}
+	}	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try{
+			String command = null;
+			CODE code;
+			while(connected && clientSocket.isConnected()){
+				infromServer =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				command = infromServer.readLine();
+				ArrayList<String> cmdList = getList(command);							
+				code = CODE.valueOf(cmdList.get(0));
+				switch(code){
+	            	case LOGINSUCC: //result LOGIN SUCCESS
+	            		System.out.println("LOGIN SUCCESS");
+	            	break;
+	            	case LOGINFAIL: //result LOGIN FAIL
+	            		System.out.println("LOGIN FAIL");
+		            break;
+	            	case LOGOUTSUCC: //result LOGOUT SUCCESS
+	            		connected = false;
+	            		closeConnect();
+	            		System.out.println("LOGOUT SUCCESS");
+	            	break;
+	            	case LOGOUTFAIL: //result LOGOUT FAIL
+	            		System.out.println("LOGOUT FAIL");
+		            break;	            	
+	            	case PAUSESUCC: //result PAUSE
+	            	break;
+	            	case PAUSEFAIL: //result PAUSE
+		            break;
+	            	case UNPAUSESUCC: //result UNPAUSE
+	            	break;
+	            	case UNPAUSEFAIL: //result UNPAUSE
+		            break;
+	            	case TRANSSUCC: //result TRANSFER	            			
+	            	break;
+	            	case TRANSFAIL: //result TRANSFER	            			
+		            break;
+	            	case HOLDSUCC: //result HOLD
+	            	break;
+	            	case HOLDFAIL: //result HOLD
+		            break;
+	            	case RESULT: //result ?
+	            	break;
+	            	case RINGING: //EVENT RINGING
+	            	break;
+	            	case AVAIL: //result 	            			
+	            	break;
+	            	case BUSY: 
+	            	break;
+	            	case READY: 
+	            	break;
+	            	case UP: //EVENT ANSWER CALL	            			
+	            	break;
+		            default: 
+	                break;
+				}
+			}
+		} catch (IOException e){
+			
+		} catch (Exception e) {
+
+		}
+	}
+
+	public static void sendtoServer(String t) throws IOException{
+		outtoServer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+		outtoServer.println(t);
+		outtoServer.flush();
+	}
+	public static ArrayList<String> getList(String cmd){
+		ArrayList<String> list =  new ArrayList<String>();
+		StringTokenizer st = new StringTokenizer(cmd,"@");
+        while(st.hasMoreTokens()){
+        	list.add(st.nextToken());
+        }
+		return list;
+	}
+	
+	public static void closeConnect()throws Exception{
+		System.out.println("start close session");
+		if(mainThread!=null){
+			mainThread.interrupt();
+			mainThread = null;
+			System.out.println("close thread");
+		}
+		if(!clientSocket.isClosed()){
+			clientSocket.close();
+			outtoServer.close();
+			infromServer.close();
+			System.out.println("close socket");
+		}
+		System.out.println("finish close session");
+	}
+	public enum CODE{
+		LOGINSUCC, LOGINFAIL, LOGOUTSUCC, LOGOUTFAIL,		
+		PAUSESUCC, PAUSEFAIL,
+		HOLDSUCC, HOLDFAIL,
+		TRANSSUCC, TRANSFAIL,
+		UNPAUSESUCC, UNPAUSEFAIL,
+		DIAL, RINGING, AVAIL, BUSY, READY, RESULT, UP,
+	}
+}
