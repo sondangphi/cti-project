14725c5914377a0fe3ca921014c95c3071c09450
haithanh.uit@gmail.com <haithanh.uit@gmail.com@de237bcd-42ee-5135-ae50-1b13d09ddbe3>
2012-10-08 08:33:20 +0000

    CTI-Client

---
+++ CTI-Client/src/org/asterisk/model/QueueObject.java
@@ -0,0 +1,30 @@
+package org.asterisk.model;
+
+import java.io.Serializable;
+
+public class QueueObject implements Serializable{
+	
+	private String queueId;
+	private String queueName;
+	
+	public QueueObject(){
+		
+	}
+	public QueueObject(String id, String name){
+		this.queueId = id;
+		this.queueName = name;
+	}	
+	public void setQueueId(String id){
+		queueId = id;
+	} 
+	public String getQueueId(){
+		return queueId;
+	}
+	public void setQueueName(String name){
+		queueName = name;
+	}
+	public String getQueueName(){
+		return queueName;
+	}
+
+}
