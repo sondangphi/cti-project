5ee5e0c261db4a53a7b0986f8b475559479af9de
haithanh.uit <haithanh.uit@gmail.com>
2012-11-01 03:23:19 +0000

    cti client

--- CTI-Client/src/org/asterisk/main/accountForm.java
+++ CTI-Client/src/org/asterisk/main/accountForm.java
@@ -8,12 +8,12 @@ package org.asterisk.main;
  *
  * @author leehoa
  */
-public class AccountForm extends javax.swing.JFrame {
+public class accountForm extends javax.swing.JFrame {
 
     /**
-     * Creates new form AccountForm
+     * Creates new form accountForm
      */
-    public AccountForm() {
+    public accountForm() {
         initComponents();
     }
 
@@ -151,20 +151,20 @@ public class AccountForm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AccountForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accountForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AccountForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accountForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AccountForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accountForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AccountForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accountForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AccountForm().setVisible(true);
+                new accountForm().setVisible(true);
             }
         });
     }
