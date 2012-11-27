0b72645a173e7f0501dd5eb5e82b00b5743b79db
haithanh.uit@gmail.com <haithanh.uit@gmail.com@de237bcd-42ee-5135-ae50-1b13d09ddbe3>
2012-11-27 02:31:51 +0000

    cti client

--- CTI-Client/src/org/asterisk/utility/Agent.java
+++ CTI-Client/src/org/asterisk/utility/Agent.java
@@ -25,7 +25,7 @@ public class Agent implements Runnable{
 	static Thread mainThread;
 	static Socket clientSocket;
         static AgentObject agentObject;
-        private static LoginForm loginf;
+        private static LoginForm loginf;        
 	int data;
 	
 	public Agent(){
@@ -57,110 +57,109 @@ public class Agent implements Runnable{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		try{
-                    String command = null;
-                    CODE code;
-                    MainForm mainForm = null;
-                    System.out.println("clientSocket.getSoTimeout \t"+clientSocket.getSoTimeout());
-//                    clientSocket.set
-                    while(connected && clientSocket.isConnected()){
-                        BufferedReader infromServer;
-                        infromServer =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        command = infromServer.readLine();
-                        ArrayList<String> cmdList = getList(command);							
-                        code = CODE.valueOf(cmdList.get(0).toUpperCase());
-                        switch(code){
-	            	case LOGINSUCC: //result LOGIN SUCCESS
-                            System.out.println("LOGIN SUCCESS");
-                            mainForm = new MainForm(this);
-                            mainForm.setVisible(true);                            
-                            loginf.setVisible(false);
-                            loginf.dispose();
-                            loginf = null;
-	            	break;
-	            	case LOGINFAIL: //result LOGIN FAIL
-                            System.out.println("LOGIN FAIL");
-                            loginf.lb_status.setText("Login Fail! Please check information again.");
-                            connected = false;
-                            closeConnect();                   
-                            try {
-                                this.finalize();
-                            } catch (Throwable ex) {
-                                Logger.getLogger(Agent.class.getName()).log(Level.SEVERE, null, ex);
+            // TODO Auto-generated method stub
+            try{
+                String command = null;
+                CODE code;
+                MainForm mainForm = null;
+                System.out.println("clientSocket.getSoTimeout \t"+clientSocket.getSoTimeout());
+                while(connected && clientSocket.isConnected()){
+                    BufferedReader infromServer;
+                    infromServer =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    command = infromServer.readLine();
+                    ArrayList<String> cmdList = getList(command);							
+                    code = CODE.valueOf(cmdList.get(0).toUpperCase());
+                    switch(code){
+                    case LOGINSUCC: //result LOGIN SUCCESS
+                        System.out.println("LOGIN SUCCESS");
+                        mainForm = new MainForm(this,agentObject);
+                        mainForm.setVisible(true);                            
+                        loginf.setVisible(false);
+                        loginf.dispose();
+                        loginf = null;
+                    break;
+                    case LOGINFAIL: //result LOGIN FAIL
+                        System.out.println("LOGIN FAIL");
+                        loginf.lb_status.setText("Login Fail! Please check information again.");
+                        connected = false;
+                        closeConnect();                   
+                        try {
+                            this.finalize();
+                        } catch (Throwable ex) {
+                            Logger.getLogger(Agent.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    break;
+                    case LOGOUTSUCC: //result LOGOUT SUCCESS
+                        System.out.println("logout");
+                        LoginForm f = new LoginForm();
+                        f.setVisible(true);
+                        mainForm.setVisible(false);
+                        mainForm.dispose();                        
+                        connected = false;                            
+                        closeConnect();  
+                        try {
+                            this.finalize();
+                        } catch (Throwable ex) {
+                            Logger.getLogger(Agent.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        System.out.println("LOGOUT SUCCESS");
+                    break;
+                    case LOGOUTFAIL: //result LOGOUT FAIL
+                        System.out.println("LOGOUT FAIL");
+                    break;	            	
+                    case PAUSESUCC: //result PAUSE
+                    break;
+                    case PAUSEFAIL: //result PAUSE
+                    break;
+                    case UNPAUSESUCC: //result UNPAUSE
+                    break;
+                    case UNPAUSEFAIL: //result UNPAUSE
+                    break;
+                    case TRANSSUCC: //result TRANSFER	            			
+                    break;
+                    case TRANSFAIL: //result TRANSFER	            			
+                    break;
+                    case HOLDSUCC: //result HOLD
+                    break;
+                    case HOLDFAIL: //result HOLD
+                    break;
+                    case RESULT: //result ?
+                    break;
+                    case RINGING: //EVENT RINGING
+                        System.out.println("RINGING");
+                        mainForm.btn_answer.setEnabled(true);
+                        mainForm.btn_hangup.setEnabled(true);
+                        mainForm.btn_pause.setEnabled(false);
+                        mainForm.btn_logout.setEnabled(false);
+                    break;
+                    case AVAIL: //result 	            			
+                    break;
+                    case BUSY: 
+                    break;
+                    case READY: 
+                    break;
+                    case HANGUP: 
+                        System.out.println("HANGUP");
+                        mainForm.btn_answer.setEnabled(false);
+                        mainForm.btn_hangup.setEnabled(false);
+                        mainForm.btn_hold.setEnabled(false);
+                        mainForm.btn_pause.setEnabled(true);
+                        mainForm.btn_logout.setEnabled(true);
+                    break;
+                    case UP: //EVENT ANSWER CALL	     
+                        System.out.println("ANSWER");
+                        mainForm.btn_hold.setEnabled(true);
+                    break;
+                    default: 
+                    break;
                             }
-                        break;
-	            	case LOGOUTSUCC: //result LOGOUT SUCCESS
-                            System.out.println("logout");
-                            LoginForm f = new LoginForm();
-                            f.setVisible(true);
-                            mainForm.setVisible(false);
-                            mainForm.dispose();
-//                            mainForm = null;                            
-                            connected = false;                            
-                            closeConnect();  
-                            try {
-                                this.finalize();
-                            } catch (Throwable ex) {
-                                Logger.getLogger(Agent.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                            System.out.println("LOGOUT SUCCESS");
-	            	break;
-	            	case LOGOUTFAIL: //result LOGOUT FAIL
-                            System.out.println("LOGOUT FAIL");
-                        break;	            	
-	            	case PAUSESUCC: //result PAUSE
-	            	break;
-	            	case PAUSEFAIL: //result PAUSE
-                        break;
-	            	case UNPAUSESUCC: //result UNPAUSE
-	            	break;
-	            	case UNPAUSEFAIL: //result UNPAUSE
-                        break;
-	            	case TRANSSUCC: //result TRANSFER	            			
-	            	break;
-	            	case TRANSFAIL: //result TRANSFER	            			
-                        break;
-	            	case HOLDSUCC: //result HOLD
-	            	break;
-	            	case HOLDFAIL: //result HOLD
-                        break;
-	            	case RESULT: //result ?
-	            	break;
-	            	case RINGING: //EVENT RINGING
-                            System.out.println("RINGING");
-                            mainForm.btn_answer.setEnabled(true);
-                            mainForm.btn_hangup.setEnabled(true);
-                            mainForm.btn_pause.setEnabled(false);
-                            mainForm.btn_logout.setEnabled(false);
-	            	break;
-	            	case AVAIL: //result 	            			
-	            	break;
-	            	case BUSY: 
-	            	break;
-	            	case READY: 
-	            	break;
-	            	case HANGUP: 
-                            System.out.println("HANGUP");
-                            mainForm.btn_answer.setEnabled(false);
-                            mainForm.btn_hangup.setEnabled(false);
-                            mainForm.btn_hold.setEnabled(false);
-                            mainForm.btn_pause.setEnabled(true);
-                            mainForm.btn_logout.setEnabled(true);
-                        break;
-	            	case UP: //EVENT ANSWER CALL	     
-                            System.out.println("ANSWER");
-                            mainForm.btn_hold.setEnabled(true);
-	            	break;
-                        default: 
-	                break;
-				}
-			}
-		} catch (IOException e){
-			
-		} catch (Exception e) {
+                    }
+            } catch (IOException e){
 
-		}
+            } catch (Exception e) {
+                System.out.println("class Agent\t"+e);
+
+            }
 	}
 
         //send request to server - string
@@ -189,17 +188,11 @@ public class Agent implements Runnable{
             }
             if(!clientSocket.isClosed()){
                 clientSocket.close();
-//                clientSocket.shutdownInput();
-//                clientSocket.shutdownOutput();
                 clientSocket = null;
                 System.out.println("close socket");
             }
             System.out.println("finish close session");            
-	}
-        
-//        public void detroy(){
-//            
-//        }
+	}        
 	public enum CODE{
             LOGINSUCC, LOGINFAIL, LOGOUTSUCC, LOGOUTFAIL,		
             PAUSESUCC, PAUSEFAIL,
